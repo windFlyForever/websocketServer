5ed7549c5b40c225aa2d0452ec7934593d587bf5
windfly <jifyzu@163.com>
2019-08-13 16:29:22 +0800

    websocket

---
+++ src/main/java/com/reman/medical/MedicalWebSocketApp.java
@@ -0,0 +1,18 @@
+package com.reman.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class MedicalWebSocketApp
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(MedicalWebSocketApp.class,args);
+    }
+}
---
+++ src/test/java/com/reman/medical/MedicalWebSocketAppTest.java
@@ -0,0 +1,20 @@
+package com.reman.medical;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MedicalWebSocketApp.
+ */
+public class MedicalWebSocketAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
